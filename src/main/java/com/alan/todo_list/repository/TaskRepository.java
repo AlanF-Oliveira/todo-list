e8aa13e2bdb9f20586f1ecf1627ddfa8acf709e0
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-15 01:10:28 -0300

    feat: add repository

---
+++ src/main/java/com/alan/todo_list/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.alan.todo_list.repository;
+
+import com.alan.todo_list.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
