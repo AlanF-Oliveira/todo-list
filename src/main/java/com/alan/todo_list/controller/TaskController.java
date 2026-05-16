8bd9e0f83356cc94a600baa7989a9c15935f054a
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-16 01:44:59 -0300

    feat: add CreateTask in controller

---
+++ src/main/java/com/alan/todo_list/controller/TaskController.java
@@ -0,0 +1,30 @@
+package com.alan.todo_list.controller;
+
+import com.alan.todo_list.dto.TaskRequest;
+import com.alan.todo_list.dto.TaskResponse;
+import com.alan.todo_list.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService service;
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> createTask(@RequestBody TaskRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createTask(request));
+    }
+
+    @GetMapping
+    public List<ResponseEntity<TaskResponse>> showAllTasks(){
+        return Res
+    }
+
+}
