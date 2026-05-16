aaec6e12874a9454e2d3c68cbade4c4de3ac02e2
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-16 01:15:11 -0300

    feat: add List mapper and createTask in Service

---
+++ src/main/java/com/alan/todo_list/service/TaskService.java
@@ -0,0 +1,30 @@
+package com.alan.todo_list.service;
+
+import com.alan.todo_list.dto.TaskRequest;
+import com.alan.todo_list.dto.TaskResponse;
+import com.alan.todo_list.entity.Task;
+import com.alan.todo_list.mapper.TaskMapper;
+import com.alan.todo_list.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class TaskService {
+
+    private final TaskMapper mapper;
+    private final TaskRepository repository;
+
+    public TaskResponse createTask(TaskRequest request){
+        Task task = mapper.toEntity(request);
+        Task taskSaved = repository.save(task);
+        return mapper.toResponse(taskSaved);
+    }
+
+    public List<TaskResponse> showAllTasks(){
+        return mapper.toResponse(repository.findAll());
+    }
+
+}
