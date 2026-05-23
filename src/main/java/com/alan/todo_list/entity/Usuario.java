3126e34b1f6d8d138164ad3b3bebea96ec5ab63f
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-23 01:19:57 -0300

    feat: add UsuarioRepository and Usuario Entity

---
+++ src/main/java/com/alan/todo_list/entity/Usuario.java
@@ -0,0 +1,36 @@
+package com.alan.todo_list.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
