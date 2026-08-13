34a875b2379235dbd8a3a9b1711ac04a2c1fbc64
AlanF-Oliveira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-08-13 04:39:55 -0300

    feat(repository): add TodoRepository with CRUD methods

---
+++ src/main/java/org/alan/todolist/repository/TodoRepository.java
@@ -0,0 +1,54 @@
+package org.alan.todolist.repository;
+
+import org.alan.todolist.model.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoRepository {
+
+    private List<Todo> list = new ArrayList<>();
+
+    public void create(Todo todo) {
+        list.add(todo);
+    }
+
+    public List<Todo> listAll() {
+        return new ArrayList<>(list);
+    }
+
+    public void delete(int id) {
+        for (Todo todo : list) {
+            if (todo.getId() == id) {
+                list.remove(todo);
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Id not found ");
+    }
+
+    public Todo findById(int id) {
+        for (Todo todo : list) {
+            if (todo.getId() == id) {
+                return todo;
+            }
+        }
+        throw new IllegalArgumentException("Id not found ");
+    }
+
+    public Todo updateTodo(int id, Todo todoRequest) {
+        Todo todo = findById(id);
+        if (todoRequest != null) {
+            todo.setName(todoRequest.getName());
+            todo.setCategory((todoRequest.getCategory()));
+            todo.setDescription(todoRequest.getDescription());
+            todo.setFinalDate(todoRequest.getFinalDate());
+            todo.setPriority(todoRequest.getPriority());
+            todo.setStatus(todoRequest.getStatus());
+
+        } else {
+            throw new NullPointerException("Parametro inválido ");
+        }
+        return todo;
+    }
+}
